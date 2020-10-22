7b10b8db9f13a571868ee17a3a6c97560aed8d89
Ansoko <ansoko@git.com>
2020-10-22 10:05:07 +0200

    test graphes

--- test.py
+++ test.py
@@ -9,3 +9,64 @@ publication_venue = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/d
 publication_year = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication_year.csv", engine='python', error_bad_lines=False)
 venue = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/venue.csv", engine='python', error_bad_lines=False)
 year = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/year.csv", engine='python', error_bad_lines=False)
+
+print(author.loc[author.name_author=='A. Andersson',])
+
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G = nx.Graph()
+G.add_nodes_from([
+    (4, {"color": "red"}),
+    (5, {"color": "green"}),
+])
+H = nx.path_graph(10)
+G.add_nodes_from(H)
+nx.draw(G)
+plt.savefig("tuto.jpg")
+plt.close()
+
+
+G = nx.Graph()
+G.add_edge(1, 2)
+e = (2, 3)
+G.add_edge(*e)  # unpack edge tuple*
+nx.draw(G)
+plt.savefig("tuto.jpg")
+plt.close()
+
+
+G = nx.grid_2d_graph(5, 5)  # 5x5 grid
+
+# print the adjacency list
+for line in nx.generate_adjlist(G):
+    print(line)
+# write edgelist to grid.edgelist
+nx.write_edgelist(G, path="grid.edgelist", delimiter=":")
+# read edgelist from grid.edgelist
+H = nx.read_edgelist(path="grid.edgelist", delimiter=":")
+nx.draw(H)
+plt.show()
+plt.savefig("filname.jpg")
+
+plt.close()
+
+G = nx.cycle_graph(24)
+pos = nx.spring_layout(G, iterations=200)
+nx.draw(G, pos, node_color=range(24), node_size=800, cmap=plt.cm.Blues)
+plt.show()
+plt.savefig("filname2.jpg")
+
+G = nx.star_graph(20)
+pos = nx.spring_layout(G)
+colors = range(20)
+options = {
+    "node_color": "#A0CBE2",
+    "edge_color": colors,
+    "width": 4,
+    "edge_cmap": plt.cm.Blues,
+    "with_labels": False,
+}
+nx.draw(G, pos, **options)
+plt.savefig("filname3.jpg")
