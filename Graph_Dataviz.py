e6c3b0e7d7dee63422367b2f92064ff6f4bc8928
Anne-Sophie Koch <45961184+Ansoko@users.noreply.github.com>
2021-03-17 10:58:03 +0100

    Update lien de l'importation des fichiers CSV

--- Graph_Dataviz.py
+++ Graph_Dataviz.py
@@ -10,15 +10,15 @@ import plotly.graph_objects as go
 from tkinter import Tk,Label,Button, Entry,IntVar,Checkbutton, Canvas
 
 #importation fichiers CSV
-author = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/author.csv", engine='python', error_bad_lines=False)
-keyword = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/keyword.csv", engine='python', error_bad_lines=False)
-publication = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication.csv", engine='python', error_bad_lines=False)
-publication_author = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication_author.csv", engine='python', error_bad_lines=False)
-publication_keyword = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication_keywords.csv", engine='python', error_bad_lines=False)
-publication_venue = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication_venue.csv", engine='python', error_bad_lines=False)
-publication_year = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/publication_year.csv", engine='python', error_bad_lines=False)
-venue = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/venue.csv", engine='python', error_bad_lines=False)
-year = pd.read_csv("C:/Users/annes/OneDrive/cours/Master 1/Projet/dataset/year.csv", engine='python', error_bad_lines=False)
+author = pd.read_csv("dataset/author.csv", engine='python', error_bad_lines=False)
+keyword = pd.read_csv("dataset/keyword.csv", engine='python', error_bad_lines=False)
+publication = pd.read_csv("dataset/publication.csv", engine='python', error_bad_lines=False)
+publication_author = pd.read_csv("dataset/publication_author.csv", engine='python', error_bad_lines=False)
+publication_keyword = pd.read_csv("dataset/publication_keywords.csv", engine='python', error_bad_lines=False)
+publication_venue = pd.read_csv("dataset/publication_venue.csv", engine='python', error_bad_lines=False)
+publication_year = pd.read_csv("dataset/publication_year.csv", engine='python', error_bad_lines=False)
+venue = pd.read_csv("dataset/venue.csv", engine='python', error_bad_lines=False)
+year = pd.read_csv("dataset/year.csv", engine='python', error_bad_lines=False)
 
 
 ##### REQUETES #####
